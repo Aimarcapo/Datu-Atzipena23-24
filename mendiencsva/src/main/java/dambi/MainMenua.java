2c4372a58fa977ab5102f66170e33fb85650e451
Aimarcapo <114486033+Aimarcapo@users.noreply.github.com>
2023-09-18 10:15:54 +0200

    Add files via upload

--- mendiencsva/src/main/java/dambi/MainMenua.java
+++ mendiencsva/src/main/java/dambi/MainMenua.java
@@ -1,5 +1,14 @@
 package dambi;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainMenua {
@@ -28,7 +37,7 @@ public class MainMenua {
                 case 3:
                     met3();
                     break;
-                
+
                 case 5:
                     System.out.println("Eskerrik asko programa hau erabiltzeagatik.");
                     break;
@@ -37,17 +46,102 @@ public class MainMenua {
             }
         } while (aukera != 5);
         in.next();
-}
+    }
 
-public static void met1(){
-    
-}
+    public static void met1() {
 
-public static void met2(){
-    
-}
-public static void met3(){
-    
-}
+        String line;
 
-}
+        try (BufferedReader br = new BufferedReader(new FileReader("Mendiak.csv"))) {
+
+            System.out.println("|------------|---------|------------|");
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                if (values.length == 3) {
+                    String mendia = values[0];
+                    String altuera = values[1];
+                    String probintzia = values[2];
+
+                    System.out.printf("| %-10s | %-7s | %-10s |\n", mendia, altuera, probintzia);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Ez da aurkitu: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void met2() {
+        String line;
+        int max = 0;
+        String biggest = "null";
+        String provin = "";
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("Mendiak.csv"))) {
+
+            while ((line = br.readLine()) != null) {
+                i++;
+                String[] values = line.split(";");
+                if (values.length == 3) {
+                    String mendia = values[0];
+                    String altuera = values[1];
+                    String probintzia = values[2];
+
+                    if (i > 1) {
+                        int numero = Integer.parseInt(altuera);
+                        if (numero > max) {
+                            max = numero;
+                            biggest = mendia;
+                            provin = probintzia;
+                        }
+                    }
+
+                }
+            }
+            System.out.println(biggest + ", " + max + ", " + provin);
+        } catch (FileNotFoundException e) {
+            System.err.println("Ez da aurkitu: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void met3() {
+        try {
+            BufferedReader inputStream = new BufferedReader(new FileReader("Mendiak.csv"));
+            
+
+            String line;
+
+            while ((line = inputStream.readLine()) != null) {
+                String[] values = line.split(";");
+                if (values.length == 3) {
+                    String province = values[2];
+                    String mendia = values[0];
+                    String altuera = values[1];
+
+                    // Write the original line to characteroutput.txt
+                
+
+                    // Write the data to the corresponding CSV file
+                    try (FileWriter provinceWriter = new FileWriter(province + ".csv", true)) {
+                        provinceWriter.write(mendia + ";" + altuera + ";" + province + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            // Close the input and output streams
+            inputStream.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+
